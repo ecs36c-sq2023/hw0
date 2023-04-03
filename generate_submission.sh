38b3e16de24bd6ffc56b288dacbb486e0c329f81
Henry Yu <hazyfish@outlook.com>
2023-04-02 17:30:34 -0700

    chore: update submission script

--- generate_submission.sh
+++ generate_submission.sh
@@ -1,2 +1,2 @@
 rm -f hw0-submission.zip
-zip -r hw0-submission.zip . -x "*.git*" "*build*" ".devcontainer*" ".vscode*" "*expected*" "*input*"
+zip -r hw0-submission.zip main.cpp CMakeLists.txt src/*
